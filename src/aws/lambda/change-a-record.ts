160581f1be73b4568cde06c64ff199df5b6f1d71
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-04-27 00:20:40 +0900

    [improve] Changed to inject dependencies into functions.

--- src/aws/lambda/change-a-record.ts
+++ src/aws/lambda/change-a-record.ts
@@ -1,13 +1,13 @@
 import * as AWS from "aws-sdk"
 
-export async function changeARecord(ip: string): Promise<AWS.Route53.ChangeResourceRecordSetsResponse> {
+export async function changeARecord(ip: string, domainName: string, hostedZoneId: string): Promise<AWS.Route53.ChangeResourceRecordSetsResponse> {
     const route53 = new AWS.Route53()
     const params: AWS.Route53.ChangeResourceRecordSetsRequest = {
         ChangeBatch: {
             Changes: [{
                 Action: "UPSERT",
                 ResourceRecordSet: {
-                    Name: "",
+                    Name: domainName,
                     ResourceRecords: [
                         {
                             Value: ip
@@ -18,7 +18,7 @@ export async function changeARecord(ip: string): Promise<AWS.Route53.ChangeResou
                 }
             }]
         },
-        HostedZoneId: ""
+        HostedZoneId: hostedZoneId
     }
 
     return await route53.changeResourceRecordSets(params).promise()
