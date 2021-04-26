ef06e0b23109e875b709a647f611c58903a03f8c
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-04-27 00:21:15 +0900

    [improve] add error handling

--- src/aws/lambda/index.ts
+++ src/aws/lambda/index.ts
@@ -2,14 +2,34 @@ import {APIGatewayProxyEvent, APIGatewayEventRequestContext, APIGatewayProxyResu
 import {changeARecord} from "./change-a-record";
 
 export async function handler(event: APIGatewayProxyEvent, context: APIGatewayEventRequestContext): Promise<APIGatewayProxyResult> {
-    const {sourceIp: clientIp} = context.identity
-
     try {
-        return await changeARecord(clientIp)
+        if (event.body == null || event.body === '') {
+            return {
+                statusCode: 400,
+                body: 'Error: illegal request(empty body)'
+            }
+        }
+
+        const {domainName} = JSON.parse(event.body)
+
+        if (domainName == null || domainName.length === 0) {
+            return {
+                statusCode: 400,
+                body: 'Error: illegal request(domain name is empty)'
+            }
+        }
+
+        const {sourceIp: clientIp} = context.identity
+        const {HOSTED_ZONE_ID} = process.env
+
+        if (HOSTED_ZONE_ID == null) throw 'Error: target hosted zone id is undefined'
+
+        return await changeARecord(clientIp, domainName, HOSTED_ZONE_ID)
             .then(() => ({
                 statusCode: 200,
                 body: ''
             }))
+
     } catch (e) {
         return {
             statusCode: 500,
