143197096a246e88d748c77349ece4f21080c508
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-04-27 23:38:53 +0900

    [improve] Add cdk deployment kit

--- lib/route53-ipv4-pusher-resource-kit-stack.ts
+++ lib/route53-ipv4-pusher-resource-kit-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import {NodejsFunction} from "@aws-cdk/aws-lambda-nodejs"
+import "dotenv/config"
 
 export class Route53Ipv4PusherResourceKitStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const {HOSTED_ZONE_ID} = process.env
+
+    if (HOSTED_ZONE_ID == null) throw 'HOSTED_ZONE_ID environment variable was not set'
+
+    const lambdaFunction = new NodejsFunction(this, 'ipv4PushFunction', {
+      entry: "src/aws/lambda/index.ts",
+      environment: {
+        HOSTED_ZONE_ID: HOSTED_ZONE_ID
+      }
+    })
   }
 }
