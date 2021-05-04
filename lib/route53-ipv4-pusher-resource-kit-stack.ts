8d842601e069e6c5e739b5ec9ac2dea3faafe0e8
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-05-04 10:24:49 +0900

    [improve] assign type annotation

--- lib/route53-ipv4-pusher-resource-kit-stack.ts
+++ lib/route53-ipv4-pusher-resource-kit-stack.ts
@@ -4,24 +4,25 @@ import {NodejsFunction} from "@aws-cdk/aws-lambda-nodejs"
 import "dotenv/config"
 import {createExecutionRole} from "./route53-ipv4-pusher-role";
 
+
 export class Route53Ipv4PusherResourceKitStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const {HOSTED_ZONE_ID} = process.env
+        const {HOSTED_ZONE_ID} = process.env
 
-    if (HOSTED_ZONE_ID == null) throw 'HOSTED_ZONE_ID environment variable was not set'
+        if (HOSTED_ZONE_ID == null) throw 'HOSTED_ZONE_ID environment variable was not set'
 
-    const lambdaFunction = new NodejsFunction(this, 'ipv4PushFunction', {
-      entry: "src/aws/lambda/index.ts",
-      environment: {
-        HOSTED_ZONE_ID: HOSTED_ZONE_ID
-      },
-      role: createExecutionRole(this)
-    })
+        const lambdaFunction: NodejsFunction = new NodejsFunction(this, 'ipv4PushFunction', {
+            entry: "src/aws/lambda/index.ts",
+            environment: {
+                HOSTED_ZONE_ID: HOSTED_ZONE_ID
+            },
+            role: createExecutionRole(this)
+        })
 
-    const gateway = new apiGateway.RestApi(this, 'ipv4PusherGateway')
+        const gateway: apiGateway.RestApi = new apiGateway.RestApi(this, 'ipv4PusherGateway')
 
-    gateway.root.addMethod('POST', new apiGateway.LambdaIntegration(lambdaFunction))
-  }
+        gateway.root.addMethod('POST', new apiGateway.LambdaIntegration(lambdaFunction))
+    }
 }
--- lib/route53-ipv4-pusher-role.ts
+++ lib/route53-ipv4-pusher-role.ts
@@ -1,10 +1,11 @@
 import * as cdk from "@aws-cdk/core";
 import * as iam from "@aws-cdk/aws-iam"
 
-export function createExecutionRole(scope: cdk.Construct): iam.IRole {
-    const AmazonRoute53FullAccessPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonRoute53FullAccess')
 
-    const role = new iam.Role(scope, 'ipv4PushRole', {
+export function createExecutionRole(scope: cdk.Construct): iam.IRole {
+    const AmazonRoute53FullAccessPolicy: iam.IManagedPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonRoute53FullAccess')
+
+    const role: iam.Role = new iam.Role(scope, 'ipv4PushRole', {
         assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
     })
 
