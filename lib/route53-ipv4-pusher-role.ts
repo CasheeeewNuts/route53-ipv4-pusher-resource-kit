c8a94dc29fc4c624721f6850438d955389252b72
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-05-04 18:12:27 +0900

    [improve] attach AWSLambdaBasicExecutionRole to allow write permission to Cloudwatch.

--- lib/route53-ipv4-pusher-role.ts
+++ lib/route53-ipv4-pusher-role.ts
@@ -3,12 +3,14 @@ import * as iam from "@aws-cdk/aws-iam"
 
 
 export function createExecutionRole(scope: cdk.Construct): iam.IRole {
+    const AWSLambdaBasicExecutionRolePolicy: iam.IManagedPolicy = iam.ManagedPolicy.fromManagedPolicyArn(scope, 'PusherBasicRole', 'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole')
     const AmazonRoute53FullAccessPolicy: iam.IManagedPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonRoute53FullAccess')
 
     const role: iam.Role = new iam.Role(scope, 'PusherRole', {
         assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
     })
 
+    role.addManagedPolicy(AWSLambdaBasicExecutionRolePolicy)
     role.addManagedPolicy(AmazonRoute53FullAccessPolicy)
 
     return role
