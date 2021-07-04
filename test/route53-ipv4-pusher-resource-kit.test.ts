2e1a8f8292feb6e42ee389c2c76734b8c330b4d2
CasheeeewNuts <nuts@pop-os.localdomain>
2021-07-04 23:15:58 +0900

    add snapshot test

--- test/route53-ipv4-pusher-resource-kit.test.ts
+++ test/route53-ipv4-pusher-resource-kit.test.ts
@@ -1,4 +1,4 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import {matchTemplate, MatchStyle, SynthUtils } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Route53Ipv4PusherResourceKit from '../lib/route53-ipv4-pusher-resource-kit-stack';
 
@@ -7,7 +7,11 @@ test('Empty Stack', () => {
     // WHEN
     const stack = new Route53Ipv4PusherResourceKit.Route53Ipv4PusherResourceKitStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot()
+    // expectCDK(stack).to(matchTemplate({
+    //   "Resources": {
+    //
+    //   }
+    // }, MatchStyle.EXACT))
 });
